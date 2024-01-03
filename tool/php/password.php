e981ad607093d78f0c740c7e7f7bd0bd8fecdfd5
nghia.duong272919136 <nghia.duong272919136@hcmut.edu.vn>
2024-01-03 19:29:03 +0700

    Create password.php

---
+++ tool/php/password.php
@@ -0,0 +1,16 @@
+
+<?php
+
+// This file is only created for better clarity, you don't have to use these functions if you want
+
+function hash_password($password)
+{
+      return password_hash($password, PASSWORD_BCRYPT);
+}
+
+function verify_password($raw_password, $hased_password)
+{
+      return password_verify($raw_password, $hased_password);
+}
+
+?>
