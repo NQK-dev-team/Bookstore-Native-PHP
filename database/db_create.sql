f4c786e83150db305252c6091e36ee74655aa753
nghia.duong272919136 <nghia.duong272919136@hcmut.edu.vn>
2024-01-04 21:41:48 +0700

    Update db_create.sql

--- database/db_create.sql
+++ database/db_create.sql
@@ -22,7 +22,7 @@ create table appUser(
     address text,
     phone varchar(10) unique,
     email varchar(100) unique,
-    password text not null,
+    password varchar(255) not null,
     check(length(password)>=8)
 );
 
