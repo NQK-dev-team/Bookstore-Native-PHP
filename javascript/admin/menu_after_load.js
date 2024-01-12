0ae49aa72f5793fd398e97afce66f2d88649c96f
astr0tas <nghia369ty@gmail.com>
2024-01-12 21:32:49 +0700

    prepare layouts for admin pages

---
+++ javascript/admin/menu_after_load.js
@@ -0,0 +1,25 @@
+$(document).ready(() =>
+{
+      const location = window.location.href;
+
+      if (location.includes('/admin/book') || location.includes('/admin/customer') || location.includes('/admin/discount'))
+      {
+            $('#manage_dropdown_0').addClass('text-primary');
+            if (location.includes('/admin/book')) $('#manage_dropdown_1').addClass('text-primary');
+            if (location.includes('/admin/customer')) $('#manage_dropdown_2').addClass('text-primary');
+            if (location.includes('/admin/discount')) $('#manage_dropdown_3').addClass('text-primary');
+      }
+      else if (location.includes('/admin/statistic'))
+      {
+            $('#statistic_dropdown_0').addClass('text-primary');
+            if (location.includes('/book')) $('#statistic_dropdown_1').addClass('text-primary');
+            if (location.includes('/customer')) $('#statistic_dropdown_2').addClass('text-primary');
+            if (location.includes('/revenue')) $('#statistic_dropdown_3').addClass('text-primary');
+      }
+      else if (location.includes('/account'))
+            $('#profile_nav').addClass('text-primary');
+      else if (location.includes('/authentication') && !location.includes('recovery'))
+            $('#signin_nav').addClass('text-primary');
+      else if (location.includes('/') && !location.includes('recovery'))
+            $('#home_nav').addClass('text-primary');
+});
--- page/admin/statistic/book/index.php
+++ page/admin/statistic/book/index.php
@@ -1,12 +1,12 @@
 <?php
-require_once __DIR__ . '/../../../tool/php/role_check.php';
+require_once __DIR__ . '/../../../../tool/php/role_check.php';
 
 if (return_navigate_error() === 400) {
       http_response_code(400);
-      require __DIR__ . '/../../../error/400.php';
+      require __DIR__ . '/../../../../error/400.php';
 } else if (return_navigate_error() === 403) {
       http_response_code(403);
-      require __DIR__ . '/../../../error/403.php';
+      require __DIR__ . '/../../../../error/403.php';
 } else {
 ?>
 
@@ -15,24 +15,24 @@ if (return_navigate_error() === 400) {
 
       <head>
             <?php
-            require_once __DIR__ . '/../../../head_element/cdn.php';
-            require_once __DIR__ . '/../../../head_element/meta.php';
+            require_once __DIR__ . '/../../../../head_element/cdn.php';
+            require_once __DIR__ . '/../../../../head_element/meta.php';
             ?>
             <link rel="stylesheet" href="/css/preset_style.css">
 
             <meta name="author" content="Nghia Duong">
-            <meta name="description" content="Manage books of NQK Bookstore">
-            <title>Manage books</title>
+            <meta name="description" content="Get statistics about books of NQK Bookstore">
+            <title>Book statistics</title>
       </head>
 
       <body>
             <?php
-            require_once __DIR__ . '/../../../layout/admin/header.php';
+            require_once __DIR__ . '/../../../../layout/admin/header.php';
             ?>
             <section id="page">
             </section>
             <?php
-            require_once __DIR__ . '/../../../layout/footer.php';
+            require_once __DIR__ . '/../../../../layout/footer.php';
             ?>
             <script src="/javascript/admin/menu_after_load.js"></script>
       </body>
