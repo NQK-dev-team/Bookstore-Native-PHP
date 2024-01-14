35e3e2b8500421c3ae9234ee5bd862a57829b956
astr0tas <nghia369ty@gmail.com>
2024-01-14 20:59:46 +0700

    update server user connection to mysql privileges for better security

--- database/db_admin.sql
+++ database/db_admin.sql
@@ -7,6 +7,6 @@ create user 'bookstore'@'localhost' identified with caching_sha2_password by 'bo
 -- Or use this line if the above one does not work
 -- create user 'bookstore'@'localhost' identified by 'bookstore123';
 
-grant all privileges on bookstore.* to 'bookstore'@'localhost';
+grant insert,select,update,delete,execute on bookstore.* to 'bookstore'@'localhost';
 
-grant file on *.* to 'bookstore'@'localhost';
+FLUSH PRIVILEGES;
