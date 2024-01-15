4e6952b86460108560cc9c35f72d4303ca0c8d5d
astr0tas <nghia369ty@gmail.com>
2024-01-16 01:34:15 +0700

    finished styling admin book manage page (i hope)

--- page/admin/book/index.php
+++ page/admin/book/index.php
@@ -97,12 +97,12 @@ if (return_navigate_error() === 400) {
                                     if ($sub_result->num_rows === 1)
                                           $elem .= "<p class='mb-0'>
                                                                   {$sub_row['name']}
-                                                                  <i class=\"bi bi-question-circle\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"{$description}\"></i>
+                                                                  <i class=\"bi bi-question-circle help\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"{$description}\"></i>
                                                                   </p>";
                                     else
                                           $elem .= "<p>
                                                                   {$sub_row['name']}
-                                                                  <i class=\"bi bi-question-circle\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"{$description}\"></i>
+                                                                  <i class=\"bi bi-question-circle help\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"{$description}\"></i>
                                                                   </p>";
                               }
                               $elem .= "</div>
@@ -125,7 +125,7 @@ if (return_navigate_error() === 400) {
                         $bookDescription = $row['description'] ? $row['description'] : 'N/A';
                         $elem .= "<td class=\"col-1 align-middle\">{$bookDescription}</td>";
                         $elem .= "<td class=\"align-middle\">
-                                                      <svg fill='#ffee00' width='24px' height='24px' viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg' class='icon' stroke='#ffee00'><g id='SVGRepo_bgCarrier' stroke-width='0'></g><g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g><g id='SVGRepo_iconCarrier'> <path d='M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z'></path> </g></svg>
+                                                      <i class=\"bi bi-star-fill text-warning\"></i>
                                                       <span>{$row['avgRating']}</span>
                                                       </td>";
 
