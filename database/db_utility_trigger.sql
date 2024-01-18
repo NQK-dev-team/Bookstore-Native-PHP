7ea07e57e3bf548bef5851a962b0f2ba23960baf
astr0tas <nghia369ty@gmail.com>
2024-01-19 01:42:33 +0700

    Update db_utility_trigger.sql

--- database/db_utility_trigger.sql
+++ database/db_utility_trigger.sql
@@ -7,7 +7,7 @@ before insert on author
 for each row
 begin
     declare newIndex int default 0;
-    select authorIdx into newIndex from author where bookID=new.id order by authorIdx desc limit 1;
+    select authorIdx into newIndex from author where bookID=new.bookID order by authorIdx desc limit 1;
     set newIndex=newIndex+1;
     set new.authorIdx=newIndex;
 end//
