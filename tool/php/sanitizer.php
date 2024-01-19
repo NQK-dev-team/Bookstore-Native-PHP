b1ce34dfc41c342cfa4ee277e558ce7062fb2c15
astr0tas <nghia369ty@gmail.com>
2024-01-20 01:30:54 +0700

    Update sanitizer.php

--- tool/php/sanitizer.php
+++ tool/php/sanitizer.php
@@ -1,16 +1,16 @@
 
 <?php
 
-function sanitize($param)
+function sanitize($param, $ignore_quotes = true)
 {
       if (!is_string($param)) return $param;
-      
+
       // Remove leading and trailing whitespaces
       $param = trim($param);
       // Remove backslashes
       $param = stripslashes($param);
       // Convert special characters to HTML entities
-      $param = htmlspecialchars($param);
+      $param = htmlspecialchars($param, $ignore_quotes ? ENT_NOQUOTES : ENT_QUOTES);
 
       return $param;
 }
