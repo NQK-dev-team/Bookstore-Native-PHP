3156fb7a29813adc00e0f985afa6dc139b167f52
NghiaDuong <nghia369ty@gmail.com>
2024-01-23 15:17:50 +0700

    fixes weird behavior in book/category lists when deleting/deactivating a book/cateogry

--- javascript/admin/book/book_list.js
+++ javascript/admin/book/book_list.js
@@ -86,109 +86,107 @@ function fetchBookList()
                   }
                   else if (data.query_result)
                   {
-                        if (data.query_result[0].length)
+                        $('#start_entry').text(data.query_result[1] ? (listOffset - 1) * entry + 1 : 0);
+                        $('#end_entry').text(listOffset * entry <= data.query_result[1] ? listOffset * entry : data.query_result[1]);
+                        $('#total_entries').text(data.query_result[1]);
+
+                        $('#prev_button').prop('disabled', listOffset === 1);
+                        $('#next_button').prop('disabled', listOffset * entry >= data.query_result[1]);
+
+                        $('#table_body').empty();
+                        for (let i = 0; i < data.query_result[0].length; i++)
                         {
-                              $('#start_entry').text(data.query_result[1] ? (listOffset - 1) * entry + 1 : 0);
-                              $('#end_entry').text(listOffset * entry <= data.query_result[1] ? listOffset * entry : data.query_result[1]);
-                              $('#total_entries').text(data.query_result[1]);
+                              const trElem = $('<tr>');
 
-                              $('#prev_button').prop('disabled', listOffset === 1);
-                              $('#next_button').prop('disabled', listOffset * entry >= data.query_result[1]);
+                              trElem.append($(`<td class=\"align-middle\">${ (listOffset - 1) * entry + i + 1 }</td>`));
+                              trElem.append(
+                                    $(`<td class=\"align-middle\"><img ${ data.query_result[0][i].imagePath } alt=\"book image\" class=\"book_image\"></img></td>`)
+                              );
+                              trElem.append($(`<td class=\"col-2 align-middle\">${ data.query_result[0][i].name }</td>`));
+                              trElem.append($(`<td class=\"align-middle\">${ data.query_result[0][i].edition }</td>`));
+                              trElem.append($(`<td class=\"align-middle\">${ data.query_result[0][i].isbn }</td>`));
+                              trElem.append($(`<td class=\"align-middle\">${ data.query_result[0][i].ageRestriction }</td>`));
 
-                              $('#table_body').empty();
-                              for (let i = 0; i < data.query_result[0].length; i++)
+                              if (data.query_result[0][i].author.length)
                               {
-                                    const trElem = $('<tr>');
-
-                                    trElem.append($(`<td class=\"align-middle\">${ (listOffset - 1) * entry + i + 1 }</td>`));
-                                    trElem.append(
-                                          $(`<td class=\"align-middle\"><img ${ data.query_result[0][i].imagePath } alt=\"book image\" class=\"book_image\"></img></td>`)
-                                    );
-                                    trElem.append($(`<td class=\"col-2 align-middle\">${ data.query_result[0][i].name }</td>`));
-                                    trElem.append($(`<td class=\"align-middle\">${ data.query_result[0][i].edition }</td>`));
-                                    trElem.append($(`<td class=\"align-middle\">${ data.query_result[0][i].isbn }</td>`));
-                                    trElem.append($(`<td class=\"align-middle\">${ data.query_result[0][i].ageRestriction }</td>`));
-
-                                    if (data.query_result[0][i].author.length)
+                                    let div = $('<div>').addClass('d-flex').addClass('flex-column');
+                                    for (let j = 0; j < data.query_result[0][i].author.length; j++)
                                     {
-                                          let div = $('<div>').addClass('d-flex').addClass('flex-column');
-                                          for (let j = 0; j < data.query_result[0][i].author.length; j++)
+                                          if (data.query_result[0][i].author.length === 1)
                                           {
-                                                if (data.query_result[0][i].author.length === 1)
-                                                {
-                                                      div.append($('<p>').addClass('mb-0').text(data.query_result[0][i].author[j]));
-                                                }
-                                                else
-                                                {
-                                                      div.append($('<p>').addClass('mb-0').text(data.query_result[0][i].author[j]));
-                                                }
+                                                div.append($('<p>').addClass('mb-0').text(data.query_result[0][i].author[j]));
                                           }
-                                          trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(div));
-                                    }
-                                    else
-                                          trElem.append($('<td>').addClass('align-middle').addClass('col-1').text('N/A'));
-
-                                    if (data.query_result[0][i].category.length)
-                                    {
-                                          let div = $('<div>').addClass('d-flex').addClass('flex-column');
-                                          for (let j = 0; j < data.query_result[0][i].category.length; j++)
+                                          else
                                           {
-                                                if (data.query_result[0][i].category.length === 1)
-                                                {
-                                                      div.append($(`<p class='mb-0'>
+                                                div.append($('<p>').addClass('mb-0').text(data.query_result[0][i].author[j]));
+                                          }
+                                    }
+                                    trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(div));
+                              }
+                              else
+                                    trElem.append($('<td>').addClass('align-middle').addClass('col-1').text('N/A'));
+
+                              if (data.query_result[0][i].category.length)
+                              {
+                                    let div = $('<div>').addClass('d-flex').addClass('flex-column');
+                                    for (let j = 0; j < data.query_result[0][i].category.length; j++)
+                                    {
+                                          if (data.query_result[0][i].category.length === 1)
+                                          {
+                                                div.append($(`<p class='mb-0'>
                                                       ${ data.query_result[0][i].category[j].name }
                                                       <i class="bi bi-question-circle help" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${ data.query_result[0][i].category[j].description ? data.query_result[0][i].category[j].description : 'N/A' }"></i>
                                                 </p>`));
-                                                }
-                                                else
-                                                {
-                                                      div.append($(`<p>
+                                          }
+                                          else
+                                          {
+                                                div.append($(`<p>
                                                       ${ data.query_result[0][i].category[j].name }
                                                       <i class="bi bi-question-circle help" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${ data.query_result[0][i].category[j].description ? data.query_result[0][i].category[j].description : 'N/A' }"></i>
                                                 </p>`));
-                                                }
                                           }
-                                          trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(div));
                                     }
-                                    else
-                                          trElem.append($('<td>').addClass('align-middle').addClass('col-1').text('N/A'));
+                                    trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(div));
+                              }
+                              else
+                                    trElem.append($('<td>').addClass('align-middle').addClass('col-1').text('N/A'));
 
-                                    trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(
-                                          $('<div>').addClass('d-flex').addClass('flex-column').append(
-                                                $('<p>').text(data.query_result[0][i].publisher)
-                                          ).append(
-                                                $('<p>').text(data.query_result[0][i].publishDate)
-                                          )
+                              trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(
+                                    $('<div>').addClass('d-flex').addClass('flex-column').append(
+                                          $('<p>').text(data.query_result[0][i].publisher)
+                                    ).append(
+                                          $('<p>').text(data.query_result[0][i].publishDate)
+                                    )
+                              ));
+
+                              trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(
+                                    $('<div>').addClass('truncate').text(data.query_result[0][i].description))
+                              );
+                              if (data.query_result[0][i].avgRating)
+                                    trElem.append($('<td>').addClass('align-middle').append(
+                                          $(`<i class=\"bi bi-star-fill text-warning me-1\"></i>`)
+                                    ).append(
+                                          $(`<span>`).text(data.query_result[0][i].avgRating)
                                     ));
-
-                                    trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(
-                                          $('<div>').addClass('truncate').text(data.query_result[0][i].description))
-                                    );
-                                    if (data.query_result[0][i].avgRating)
-                                          trElem.append($('<td>').addClass('align-middle').append(
-                                                $(`<i class=\"bi bi-star-fill text-warning me-1\"></i>`)
-                                          ).append(
-                                                $(`<span>`).text(data.query_result[0][i].avgRating)
-                                          ));
-                                    else
-                                          trElem.append($('<td>').addClass('align-middle').text('N/A'));
-                                    trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(
-                                          $(`<div class="d-flex flex-column">`).append(
-                                                $(`<p>Physical: ${ data.query_result[0][i].physicalCopy.price } (in stock: ${ data.query_result[0][i].physicalCopy.inStock })</p>`)
-                                          ).append(
-                                                $(`<p>E-book: ${ data.query_result[0][i].fileCopy.price } <a ${ data.query_result[0][i].fileCopy.filePath !== '' ? "target='_blank'" : '' } ${ data.query_result[0][i].fileCopy.filePath } alt='${ data.query_result[0][i].fileCopy.filePath !== '' ? 'PDF file' : 'No PDF file' }'>
+                              else
+                                    trElem.append($('<td>').addClass('align-middle').text('N/A'));
+                              trElem.append($('<td>').addClass('align-middle').addClass('col-1').append(
+                                    $(`<div class="d-flex flex-column">`).append(
+                                          $(`<p>Physical: ${ data.query_result[0][i].physicalCopy.price } (in stock: ${ data.query_result[0][i].physicalCopy.inStock })</p>`)
+                                    ).append(
+                                          $(`<p>E-book: ${ data.query_result[0][i].fileCopy.price } <a ${ data.query_result[0][i].fileCopy.filePath !== '' ? "target='_blank'" : '' } ${ data.query_result[0][i].fileCopy.filePath } alt='${ data.query_result[0][i].fileCopy.filePath !== '' ? 'PDF file' : 'No PDF file' }'>
                                           <i class=\"bi bi-file-earmark-fill text-secondary\" data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"${ data.query_result[0][i].fileCopy.filePath !== '' ? 'Read file' : 'No PDF file' }\"></i>
                                           </a></p>`)
-                                          )
-                                    ));
+                                    )
+                              ));
 
-                                    trElem.append(
-                                          $(`<td class='align-middle'>
+                              trElem.append(
+                                    $(`<td class='align-middle'>
                                                       <div class='d-flex flex-lg-row flex-column'>
                                                             <a class='btn btn-info btn-sm' href='./edit-book?id=${ data.query_result[0][i].id }' data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"Edit\">
                                                                   <i class=\"bi bi-pencil text-white\"></i>
                                                             </a>
-                                                            <button data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"${ status ? 'Deactivate' : 'Activate' }\" onclick='${ status ? 'confirmDeactivateBook' : 'confirmActivateBook' }(\"${ data.query_result[0][i].id }\")' class='btn ${ status ? 'btn-danger' : 'btn-success' } ms-lg-2 mt-2 mt-lg-0 btn-sm' data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"${ status ? 'Deactive' : 'Activate' }\">
+                                                            <button data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"${ status ? 'Deactivate' : 'Activate' }\" onclick='${ status ? 'confirmDeactivateBook' : 'confirmActivateBook' }(\"${ data.query_result[0][i].id }\")' class='btn ${ status ? 'btn-danger' : 'btn-success' } ms-lg-2 mt-2 mt-lg-0 btn-sm'>
                                                                   <i class="bi bi-power text-white"></i>
                                                             </button>
                                                             ${ data.query_result[0][i].can_delete ? `<button data-bs-toggle=\"tooltip\" data-bs-placement=\"top\" data-bs-title=\"Delete\" onclick='confirmDeleteBook(\"${ data.query_result[0][i].id }\")' class='btn btn-danger ms-lg-2 mt-2 mt-lg-0 btn-sm'>
@@ -197,12 +195,12 @@ function fetchBookList()
                                                       </div>
                                                 </td>`));
 
-                                    $('#table_body').append(trElem);
-                              }
-
-                              initToolTip();
+                              $('#table_body').append(trElem);
                         }
-                        else
+
+                        initToolTip();
+
+                        if (listOffset > 1 && !data.query_result[0].length)
                         {
                               changeList(false);
                         }
@@ -247,7 +245,7 @@ function changeList(isNext)
       else
       {
             $('#next_button').prop('disabled', false);
-            $('#list_offset').text(currentOffset - 1);
+            $('#list_offset').text((currentOffset - 1) ? currentOffset - 1 : 1);
             $('#prev_button').prop('disabled', currentOffset <= 2);
       }
       fetchBookList();
