1bd976ff12828ec4d251f5e1fb9586b5f8afb376
hikaric <minhquang200257@gmail.com>
2024-01-24 10:26:01 +0700

    Update index.php

--- page/customer/index.php
+++ page/customer/index.php
@@ -24,10 +24,22 @@ if (return_navigate_error() === 400) {
                   exit;
             }
             $elem = $conn->prepare('select book.name, author.authorName, fileCopy.price as filePrice, physicalCopy.price as physicalPrice from book inner join author on book.id = author.bookID
-                                                                                                                                                          join fileCopy on book.id = fileCopy.id
-                                                                                                                                                          join physicalCopy on book.id = physicalCopy.id');
+                                                                                                                                                                                  join fileCopy on book.id = fileCopy.id
+                                                                                                                                                                                  join physicalCopy on book.id = physicalCopy.id');
             $elem->execute();
             $elem = $elem->get_result();
+
+            $featured = $conn->prepare('select distinct physicalOrders.bookID, pSales, fSales, (pSales + fSales)  as sales, book.name, author.authorName, fileCopy.price as filePrice, physicalCopy.price as physicalPrice from customerOrder join (select sum(amount) as pSales, physicalOrder.id as id, bookID from physicalOrder join physicalOrderContain on physicalOrder.id = physicalOrderContain.orderID group by bookID) as physicalOrders on customerOrder.id = physicalOrders.id 
+                                                                                                                                                                                                                                              join (select count(orderID) as fSales, fileOrder.id as id from fileOrder join fileOrderContain on fileOrder.id = fileOrderContain.orderID group by bookID) as fileOrders on customerOrder.id = fileOrders.id
+                                                                                                                                                                                                                                              join author on physicalOrders.bookID = author.bookID
+                                                                                                                                                                                                                                              join book on book.id = physicalOrders.bookID
+                                                                                                                                                                                                                                              join fileCopy on book.id = fileCopy.id
+                                                                                                                                                                                                                                              join physicalCopy on book.id = physicalCopy.id
+                                                                                                                                                                                                                           order by sales DESC
+                                                                                                                                                                                                                           limit 3');
+            $featured->execute();
+            $featured = $featured->get_result();
+
             $conn->close();
       }
       catch (Exception $e){
@@ -82,6 +94,30 @@ if (return_navigate_error() === 400) {
                   <div>
                         <h1 class="text-center">Welcome to our shop</h1>
                         <h2>Featured books</h2>
+                        <?php
+                                    if($featured->num_rows > 0){
+                                          echo"<div class=\"grid-container\">";
+                                          while($row=$featured->fetch_assoc()){
+                                                // insert a card for link here
+                                                 echo "<div class=\"card mb-3 border-light\">";
+                                                      // insert picture here
+                                                      echo "<img class=\"pic\" src=\"https://static.vecteezy.com/system/resources/thumbnails/002/219/582/small_2x/illustration-of-book-icon-free-vector.jpg\">";
+                                                      echo "<div class=\"card-body\">";
+                                                            echo "<h5 class=\"card-title\">"."Book: ".$row["name"]."</h5>";
+                                                            echo "<p class=\"author\">".$row["authorName"]."</p>";
+                                                            echo "<p class=\"price\">"."E-book price: ".$row["filePrice"]."$"."</p>";
+                                                            echo "<p class=\"price\">"."Physical price: ".$row["physicalPrice"]."$"."</p>";
+                                                            // echo "<p>"."Ratings: ".$row["stars"]."/5"."</p>";
+                                                            // insert ratings here
+                                                      echo "</div>";
+                                                echo "</div>";
+                                           }
+                                          echo "</div>";
+                                    }
+                                    else{
+                                          echo "Some error occured!";
+                                    }
+                              ?>
                         <h2>Browse book</h2>
                               <?php
                                     if($elem->num_rows > 0){
@@ -96,6 +132,7 @@ if (return_navigate_error() === 400) {
                                                             echo "<p class=\"author\">".$row["authorName"]."</p>";
                                                             echo "<p class=\"price\">"."E-book price: ".$row["filePrice"]."$"."</p>";
                                                             echo "<p class=\"price\">"."Physical price: ".$row["physicalPrice"]."$"."</p>";
+                                                            // echo "<p>"."Ratings: ".$row["stars"]."/5"."</p>";
                                                             // insert ratings here
                                                       echo "</div>";
                                                 echo "</div>";
