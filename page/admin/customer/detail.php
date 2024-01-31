66fdff43e192abc7ce65acef997d8bf51a8da4c7
astr0tas <nghia369ty@gmail.com>
2024-01-31 12:41:01 +0700

    making preparation for customer detailed information page

---
+++ page/admin/customer/detail.php
@@ -0,0 +1,48 @@
+<?php
+require_once __DIR__ . '/../../../tool/php/login_check.php';
+require_once __DIR__ . '/../../../tool/php/role_check.php';
+
+if (return_navigate_error() === 400) {
+      http_response_code(400);
+      require_once __DIR__ . '/../../../error/400.php';
+} else if (return_navigate_error() === 403) {
+      http_response_code(403);
+      require_once __DIR__ . '/../../../error/403.php';
+} else {
+      require_once __DIR__ . '/../../../tool/php/anti_csrf.php';
+
+      $_SESSION['update_book_id'] = null;
+?>
+
+      <!DOCTYPE html>
+      <html>
+
+      <head>
+            <?php
+            require_once __DIR__ . '/../../../head_element/cdn.php';
+            require_once __DIR__ . '/../../../head_element/meta.php';
+            ?>
+            <link rel="stylesheet" href="/css/preset_style.css">
+
+            <meta name="author" content="Nghia Duong">
+            <meta name="description" content="Manage a customer detailed information of NQK Bookstore">
+            <title>Customer Detail</title>
+            <?php storeToken(); ?>
+      </head>
+
+      <body>
+            <?php
+            require_once __DIR__ . '/../../../layout/admin/header.php';
+            ?>
+            <section id="page">
+            </section>
+            <?php
+            require_once __DIR__ . '/../../../layout/footer.php';
+            ?>
+            <script src="/javascript/admin/menu_after_load.js"></script>
+            <script src="/javascript/admin/customer/order_list.js"></script>
+      </body>
+
+      </html>
+
+<?php } ?>
