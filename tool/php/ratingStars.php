99cd563797288fbd926cfd0d04848e19c4eadade
KhoaLiang <akhoa2020@gmail.com>
2024-02-02 11:36:10 +0700

    fix rating php
    use return instead of echo

--- tool/php/ratingStars.php
+++ tool/php/ratingStars.php
@@ -1,74 +1,79 @@
 <?php
 function displayRatingStars($avgRating) {
     if($avgRating <1){
-        echo '<i class="bi bi-star-half"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
+        return' <i class="bi bi-star-half"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 1 && $avgRating <1.5){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
+      return ' <i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 1.5 && $avgRating <2){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-half"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
+      return ' <i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-half"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 2 && $avgRating <2.5){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
+      return ' <i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 2.5 && $avgRating <3){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-half"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
+      return '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-half"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 3 && $avgRating <3.5){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star"></i>';
-        echo '<i class="bi bi-star"></i>';
+      return '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star"></i>'.
+         '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 3.5 && $avgRating <4){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-half"></i>';
-        echo '<i class="bi bi-star"></i>';
+      return '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-fill"></i>'.
+         '<i class="bi bi-star-half"></i>'.
+         '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 4 && $avgRating <4.5){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star"></i>';
+      return      '<i class="bi bi-star-fill"></i>'.
+                  '<i class="bi bi-star-fill"></i>'.
+                  '<i class="bi bi-star-fill"></i>'.
+                  '<i class="bi bi-star-fill"></i>'.
+                  '<i class="bi bi-star"></i>';
+      //   echo '<i class="bi bi-star-fill"></i>';
+      //   echo '<i class="bi bi-star-fill"></i>';
+      //   echo '<i class="bi bi-star-fill"></i>';
+      //   echo '<i class="bi bi-star-fill"></i>';
+      //   echo '<i class="bi bi-star"></i>';
   }
   elseif($avgRating >= 4.5 && $avgRating <4.8){
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-fill"></i>';
-        echo '<i class="bi bi-star-half"></i>';
+      return '<i class="bi bi-star-fill"></i>'.
+      '<i class="bi bi-star-fill"></i>'.
+      '<i class="bi bi-star-fill"></i>'.
+      '<i class="bi bi-star-fill"></i>'.
+      '<i class="bi bi-star-half"></i>';
   }
   elseif($avgRating >= 4.8){
-      echo '<i class="bi bi-star-fill"></i>';
-      echo '<i class="bi bi-star-fill"></i>';
-      echo '<i class="bi bi-star-fill"></i>';
-      echo '<i class="bi bi-star-fill"></i>';
-      echo '<i class="bi bi-star-fill"></i>';
+      return '<i class="bi bi-star-fill"></i>'.
+       '<i class="bi bi-star-fill"></i>'.
+       '<i class="bi bi-star-fill"></i>'.
+       '<i class="bi bi-star-fill"></i>'.
+       '<i class="bi bi-star-fill"></i>';
   }
 }
 ?>
