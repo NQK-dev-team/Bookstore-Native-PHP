16842c4bd6e9b061412495f94429204e21310636
KhoaLiang <akhoa2020@gmail.com>
2024-03-25 15:18:29 +0700

    fix feature book p1

--- page/customer/index.php
+++ page/customer/index.php
@@ -62,19 +62,13 @@ if ($return_status_code === 400) {
             ?>
             <link rel="stylesheet" href="/css/preset_style.css">
 
-            <meta name="author" content="Quang Nguyen">
+            <meta name="author" content="Khoa">
             <meta name="description" content="Home page of NQK bookstore">
             <title>NQK Shop</title>
 
             <style>
-                  .grid-container {
-                        display: grid;
-                        grid-template-columns: auto auto auto auto;
-                        justify-content: space-evenly;
-                        align-content: center;
-                  }
                   .card:hover {
-                        transform: scale(1.1);
+                        transform: scale(1.05);
                   } 
                   .author {
                         color: gray;
@@ -109,99 +103,92 @@ if ($return_status_code === 400) {
             require_once __DIR__ . '/../../layout/customer/header.php';
             ?>
             <section id="page">
-                  <div>
-                        <h1 class="text-center heading-decord">Welcome to our shop!</h1>
-                        <h2 class="text-center heading-decord">Featured books</h2>
+            <br>
+      <?php
+            if($featured->num_rows > 0){
+                  echo '<div class= "container border border-dark rounded bgr-col">';
+                  echo '<p class="h1">Featured books</p>';
+                  echo '<hr>';
+                  echo '<div class="row justify-content-center align-items-center g-2 m-3 p-1">';
+                  for($i = 1; $i <= $featured->num_rows; $i++){
+                        $row=$featured->fetch_assoc();
+                        if($i < 3){
+                         $imagePath = "https://{$_SERVER['HTTP_HOST']}/data/book/" . normalizeURL(rawurlencode($row['pic']));
+                        echo ' <div class="col-9 col-md-6">';
+                        echo "<a href=\"book/book-detail?bookID=".normalizeURL(rawurlencode($row["id"]))."\">"; 
+                        echo'                  <div class="card mx-2">
+                                                <div class="row g-0">
+                                                      <div class="col-md-7 d-flex justify-content-center ">';
+                        echo '<img src="' . $imagePath . '" class="card-img my-3 px-3" style="height: 28rem;" alt="...">';
+                        echo '
+                              </div>
+                              <div class="col-md-5">
+                                    <div class="card-body" style="max-height: 350px;">
+                                    <h5 class="card-title">'.$row["name"].'</h5>
+                                    <p class="author">'.$row["authorName"].'</p>';
+                                    echo "<p class=\"price\">"."E-book price: ".$row["filePrice"]."$"."</p>";
+                        echo "<p class=\"price\">"."Physical price: ".$row["physicalPrice"]."$"."</p>";
+                        echo '<span class="text-warning">'.displayRatingStars($row["star"]).'</span>';
+                        echo "(".$row["star"].")";
+                        echo '</div>
+                              </div>
+                              </div>
+                        </div>';
+                        echo '</a>
+                                    </div>'; //end col-9 col-md-4
+                        }
                         
-                              <?php
-                                    if($featured->num_rows > 0){
-                                          echo '<div class= "container border border-dark rounded bgr-col">';
-                                          echo '<div class="row justify-content-center align-items-center g-2 m-3 p-1">';
-                                          while($row=$featured->fetch_assoc()){
-                                                // insert a card for link here
-                                                echo '<div class="col-9 col-md-4">';
-                                                $imagePath = "https://{$_SERVER['HTTP_HOST']}/data/book/" . normalizeURL(rawurlencode($row['pic']));
-                                                echo '<div class="card w-75 mx-auto d-block">';
-                                                echo "<a href=\"book/book-detail?bookID=".normalizeURL(rawurlencode($row["id"]))."\">"; 
-                                                echo '<img src="' . $imagePath . '" class="card-img-top" style="height: 28rem;" alt="...">';
-                                                     echo "<div class=\"card-body\">";
-                                                           echo "<h5 class=\"card-title\">"."Book: ".$row["name"]."</h5>";
-                                                           echo "<p class=\"author\">".$row["authorName"]."</p>";
-                                                           echo "<p class=\"price\">"."E-book price: ".$row["filePrice"]."$"."</p>";
-                                                           echo "<p class=\"price\">"."Physical price: ".$row["physicalPrice"]."$"."</p>";
-                                                           // $cnt = 1;
-                                                           // $res="";
-                                                           // while($cnt <= 5){
-                                                           //       if ($cnt > $row["star"]){
-                                                           //             if($cnt - $row["star"] > 0 && $cnt - $row["star"] < 1){
-                                                           //                   $res .= "<i class=\"bi bi-star-half\"></i>";
-                                                           //             }
-                                                           //             else{
-                                                           //                   $res .= "<i class=\"bi bi-star\"></i>";
-                                                           //             }
-                                                           //       }
-                                                           //       else {
-                                                           //             $res .= "<i class=\"bi bi-star-fill\"></i>";
-                                                           //       }
-                                                           //       $cnt++;
-                                                           // }
-                                                           echo '<span class="text-warning">'.displayRatingStars($row["star"]).'</span>';
-                                                           echo "(".$row["star"].")";
-                                                           
-                                                     echo "</div>";
-                                               echo "</a>";
-                                               echo "</div>";
-
-                                                echo "</div>";
-                                           }
-                                           echo "</div>";
-                                          echo "</div>";
-                                    }
-                                    else{
-                                          echo "Some error occured!";
-                                    }
-                              ?>
-                        <h2 class="text-center heading-decord">Browse book</h2>
-                              <?php
-                              echo '<div class="container border border-dark rounded bgr-col mb-3">';
-                                    for ($i = 1; $i <= $elem->num_rows; $i++) {
-                                          if ($i % 4 == 1) {
-                                                echo '<div class="row justify-content-center align-items-center g-2 m-3">';
-                                          }
-                                          echo '<div class="col-9 col-md-3">';
-                                          $row = $elem->fetch_assoc();
-                                          // $row["pic"] = "src=\"https://{$_SERVER['HTTP_HOST']}/data/book/" . normalizeURL(rawurlencode($row["pic"])) . "\"";
-                                          $imagePath = "https://{$_SERVER['HTTP_HOST']}/data/book/" . normalizeURL(rawurlencode($row['pic']));
-                                                                        echo '<div class="card w-75 mx-auto d-block ">';
-                                                                         echo "<a href=\"book/book-detail?bookID=".normalizeURL(rawurlencode($row["id"]))."\">"; 
-                                                                         echo '<img src="' . $imagePath . '" class="card-img-top" style="height: 28rem;" alt="...">';
-                                                                              echo "<div class=\"card-body\">";
-                                                                                    echo "<h5 class=\"card-title\">"."Book: ".$row["name"]."</h5>";
-                                                                                    echo "<p class=\"author\">".$row["authorName"]."</p>";
-                                                                                    echo "<p class=\"price\">"."E-book price: ".$row["filePrice"]."$"."</p>";
-                                                                                    echo "<p class=\"price\">"."Physical price: ".$row["physicalPrice"]."$"."</p>";
-                                                                                    echo '<span class="text-warning">'.displayRatingStars($row["star"]).'</span>';
-                                                                                    echo "(".$row["star"].")";
-                                                                                    
-                                                                              echo "</div>";
-                                                                        echo "</a>";
-                                                                        echo "</div>";
+                  }
+                        echo "</div>"; //row end
                         
-                                          echo '</div>';
-                                          if ($i % 4 == 0 || $i == $elem->num_rows) {
-                                                echo '</div>';
-                                          }
-                                          }
-                                          echo '<a 
-                                          name=""
-                                          id=""
-                                          class="btn btn-primary d-flex justify-content-center align-items-center w-25 mx-auto m-3"
-                                          href="book"
-                                          role="button"
-                                          >Learn more</a>';
-                                          echo '</div>';
-                                          
-                              ?>
+                  echo "</div>";  //container end
+                  
+            }
+            else{
+                  echo "Some error occured!";
+            }
+      ?>
+      <h2 class="text-center heading-decord">Browse book</h2>
+            <?php
+            echo '<div class="container border border-dark rounded bgr-col mb-3">';
+                  for ($i = 1; $i <= $elem->num_rows; $i++) {
+                        if ($i % 4 == 1) {
+                              echo '<div class="row justify-content-center align-items-center g-2 m-3">';
+                        }
+                        echo '<div class="col-9 col-md-3">';
+                        $row = $elem->fetch_assoc();
+                        // $row["pic"] = "src=\"https://{$_SERVER['HTTP_HOST']}/data/book/" . normalizeURL(rawurlencode($row["pic"])) . "\"";
+                        $imagePath = "https://{$_SERVER['HTTP_HOST']}/data/book/" . normalizeURL(rawurlencode($row['pic']));
+                                                      echo '<div class="card w-75 mx-auto d-block ">';
+                                                            echo "<a href=\"book/book-detail?bookID=".normalizeURL(rawurlencode($row["id"]))."\">"; 
+                                                            echo '<img src="' . $imagePath . '" class="card-img-top" style="height: 28rem;" alt="...">';
+                                                            echo "<div class=\"card-body\">";
+                                                                  echo "<h5 class=\"card-title\">"."Book: ".$row["name"]."</h5>";
+                                                                  echo "<p class=\"author\">".$row["authorName"]."</p>";
+                                                                  echo "<p class=\"price\">"."E-book price: ".$row["filePrice"]."$"."</p>";
+                                                                  echo "<p class=\"price\">"."Physical price: ".$row["physicalPrice"]."$"."</p>";
+                                                                  echo '<span class="text-warning">'.displayRatingStars($row["star"]).'</span>';
+                                                                  echo "(".$row["star"].")";
+                                                                  
+                                                            echo "</div>";
+                                                      echo "</a>";
+                                                      echo "</div>";
+      
+                        echo '</div>';
+                        if ($i % 4 == 0 || $i == $elem->num_rows) {
+                              echo '</div>';
+                        }
+                        }
+                        echo '<a 
+                        name=""
+                        id=""
+                        class="btn btn-primary d-flex justify-content-center align-items-center w-25 mx-auto m-3"
+                        href="book"
+                        role="button"
+                        >Learn more</a>';
+                        echo '</div>';
+                        
+            ?>
                   </div>
             </section>
             <?php
